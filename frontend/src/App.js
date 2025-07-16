3c30d255b0480fe59e03bf3c1c0d7198971e815d
sohel1415 <chaudharisohel1415@gmail.com>
2025-07-17 02:18:26 +0530

    initial frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header">
+          <h1>Sweet Shop Management</h1>
+        </div>
+        <div className="navbar">
+          <a href="#" >Home</a>
+          <a href="#" >Add Sweet</a>
+          <a href="#" >All Sweet</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
