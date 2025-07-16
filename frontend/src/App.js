da0d17961df30a921ecf66931cba04f7d75cd3a6
sohel1415 <chaudharisohel1415@gmail.com>
2025-07-17 02:56:36 +0530

    Add Sweet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,34 @@ function App() {
           <a href="#" >Add Sweet</a>
           <a href="#" >All Sweet</a>
         </div>
+        <div className="content">
+          <AddSweet />
+        </div>
       </div>
+    </div>
+  );
+}
+
+function AddSweet() {
+  return (
+    <div className="purchase-form">
+      <h3>Add Sweets</h3>
+      <form>
+        <label for="sweetType">Sweet Type:</label>
+        <select id="sweetType" name="sweetType" required>
+          <option value="">--Select Sweet Type--</option>
+          <option value="chocolate">Chocolate</option>
+          <option value="candy">Candy</option>
+          <option value="cake">Cake</option>
+          <option value="pastry">Pastry</option>
+        </select>
+        <label for="sweetName">Sweet Name:</label>
+        <input type="text" id="sweetName" name="sweetName" required />
+        <label for="quantity">Quantity:</label>
+        <input type="number" id="quantity" name="quantity" min="1" required />
+        <button type="submit">Add Sweet</button>
+      </form>
+
     </div>
   );
 }
